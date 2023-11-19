6fa90d50a848da7904c9021ca4b9f28ce4b8779a
drRamzywork <drramzywork@gmail.com>
2023-11-19 15:08:55 +0200

    ..

--- components/SVGParts/Svg.jsx
+++ components/SVGParts/Svg.jsx
@@ -6,7 +6,8 @@ import GElements from './GElements';
 const Svg = () => {
   const [viewBox, setViewBox] = useState()
   const [smallScreen, setSmallScreen] = useState(false)
-
+  const [width, setWidth] = useState();
+  const [height, setHeight] = useState()
   useEffect(() => {
     const handleResize = () => {
       const viewportWidth = window.innerWidth;
@@ -14,15 +15,22 @@ const Svg = () => {
         setSmallScreen(true)
       } else {
         setSmallScreen(false)
-
       }
 
       if (viewportWidth <= 600) {
-        setViewBox("90 70 758 624"); // for small screens
+        setViewBox("0 0 414 407"); // for small screens
+        setWidth("414")
+        setHeight("407")
       } else if (viewportWidth <= 1200) {
         setViewBox("0 0 858 724"); // for medium screens
+        setWidth("858")
+        setHeight("724")
+
       } else {
         setViewBox("0 0 858 724"); // for large screens
+        setWidth("858")
+        setHeight("724")
+
       }
     };
 
@@ -40,12 +48,9 @@ const Svg = () => {
   return (
     <>
       <xml version="1.0" encoding="UTF-8" standalone="no" />
-      <svg id="svg1" width="858" height="724" fill="none" xmlns="http://www.w3.org/2000/svg" class="saudi-map" viewBox="0 0 858 724">
+      <svg id="svg1" width={width} height={height} fill="none" xmlns="http://www.w3.org/2000/svg" class="saudi-map" viewBox={viewBox}>
         <GElements />
       </svg>
-
-
-
     </>
   )
 }
